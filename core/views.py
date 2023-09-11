a63f1d5dc91057c0b20579adf70de1d077ec306e
*_* <126816247+RaphaRRP@users.noreply.github.com>
2023-09-11 11:23:15 -0300

    Add files via upload

---
+++ core/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .forms import ContatoForm
+from django.contrib import messages
+
+def index(request):
+    return render(request, 'index.html')
+
+def contato(request):
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print('Mensagem enviada')
+            print(f'Nome: ', nome)
+            print(f'Email: ', email)
+            print(f'Assunto: ', assunto)
+            print(f'mensagem: ', mensagem)
+
+            messages.success(request, 'Mensagem enviado com sucesso! ')
+            form = ContatoForm()
+
+        else:
+            messages.error(request, 'erro ao enviar E-mail!')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
+
+def produto(request):
+    return render(request, 'produto.html')
